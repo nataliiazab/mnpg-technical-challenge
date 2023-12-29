2c44249e8aeda89ba3c495aac131e4beda1b2261
nataliiazab <113237479+nataliiazab@users.noreply.github.com>
2023-12-29 18:47:05 +0000

    initial fetch of data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import Card from "./components/Card";
 
 const App = () => {
+  const url = "https://moonpig.github.io/tech-test-frontend/search.json";
+  const [data, setData] = useState([]);
+  const fetchInfo = () => {
+    return fetch(url)
+      .then((res) => res.json())
+      .then((d) => setData(d));
+  };
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
 
   return (
     <div className="App">
-    My app
+      <h1>fetch API</h1>
+      {console.log(data)}
+      <p>Number Of Products: {data.NumberOfProducts}</p>
+      {data.Products &&
+        data.Products.map((product) => (
+          <Card
+            key={product.ProductId} // Use a unique key for each product
+            title={product.Title}
+            price={product.Price.Value}
+            description={product.Description}
+            imageUrl={product.ProductImage.Link.Href}
+          />
+        ))}
     </div>
   );
 };
---
+++ src/components/Card.js
@@ -0,0 +1,5 @@
+const Card = ({ key, title, price, description }) => {
+  return <div className="card-container"> Title {title}</div>;
+};
+
+export default Card;
