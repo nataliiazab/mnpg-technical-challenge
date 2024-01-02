881583835dc6aafeebacd205e1198bb206842951
nataliiazab <113237479+nataliiazab@users.noreply.github.com>
2024-01-02 20:05:19 +0000

    add simple jest test for app.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import "@testing-library/jest-dom/extend-expect";
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders number of products", async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // to wait for the component to fetch data (asynchronous)
+  await screen.findByText(/Number Of Products:/i);
+
+  // To check if the "Number Of Products" text is present
+  expect(screen.getByText(/Number Of Products:/i)).toBeInTheDocument();
 });
