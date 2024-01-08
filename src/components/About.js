71ff4ff1739f449e5bdf01481fd897fdc6639cf5
nataliiazab <113237479+nataliiazab@users.noreply.github.com>
2024-01-08 22:26:54 +0000

    add icons to about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,18 @@
 import "../assets/styles/About.css";
+import { LuPiggyBank } from "react-icons/lu";
+import { GiCardPick, GiFlowers } from "react-icons/gi";
+import { IoIosGift } from "react-icons/io";
 
 const About = () => {
   return (
     <div className="about-container">
-      <h2>About Us</h2>
+      <div className="about-heading">
+        <div className="about-icon piggy">
+          {" "}
+          <LuPiggyBank />
+        </div>
+        <h1>About Us</h1>
+      </div>
       <p>
         At Moonpig, we’re all about celebrating those heartfelt life moments.
         You know the ones we mean…the big birthdays, new arrivals, surprise
@@ -15,7 +24,14 @@ const About = () => {
         it’s never been easier to send a card from the palm of your hand and
         share some love wherever you are.
       </p>
-      <h3>We’ve Got a Card for That</h3>
+
+      <div className="about-heading">
+        <div className="about-icon card">
+          {" "}
+          <GiCardPick />
+        </div>
+        <h2>We’ve Got a Card for That</h2>
+      </div>
       <p>
         With thousands of designs, both personalised and ready-made, choose the
         one that’s just right for you and your loved one. Got an inside joke?
@@ -27,7 +43,12 @@ const About = () => {
         matter what the occasion or who you’re looking for, we’ve got a card for
         that.
       </p>
-      <h3>Aaaaand a Gift?</h3>
+      <div className="about-heading">
+        <div className="about-icon gift">
+          <IoIosGift />
+        </div>
+        <h2>Aaaaand a Gift?</h2>
+      </div>
       <p>
         We know a thing or two when it comes to helping you find the perfect
         gift to go with your card, and using tech to show you a selection of
@@ -35,7 +56,13 @@ const About = () => {
         joyously curated selection of treats to surprise your favourites...
         whether it’s in person, or through their letterbox.
       </p>
-      <h3>Beautiful Blooms</h3>
+      <div className="about-heading">
+        <div className="about-icon flowers">
+          {" "}
+          <GiFlowers />
+        </div>
+        <h2>Beautiful Blooms</h2>
+      </div>
       <p>
         From blossom to bouquet, every step of your flowers’ journey is handled
         with care. They arrive at their new home in bud to preserve freshness,
